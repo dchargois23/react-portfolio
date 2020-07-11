43d4df10a93eaff7956b3bdf4c58dda11692cd00
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-07-10 22:46:46 -0500

    Updates

---
+++ precache-manifest.d06018d1fa55f29db00b312a8028b319.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb73489a644619bf00f1ef7c3a0482da",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9e678f16f1dc379611a7",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3432c4161bbf276911f6",
+    "url": "/react-portfolio/static/css/main.6b627099.chunk.css"
+  },
+  {
+    "revision": "9e678f16f1dc379611a7",
+    "url": "/react-portfolio/static/js/2.9f8e9770.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.9f8e9770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3432c4161bbf276911f6",
+    "url": "/react-portfolio/static/js/main.b354f3a4.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "a90b229a8d8f502e04086761f484f9e0",
+    "url": "/react-portfolio/static/media/Fitness Tracker.a90b229a.png"
+  },
+  {
+    "revision": "18252fecc0d3c303527d03450fc41eef",
+    "url": "/react-portfolio/static/media/Note Taker.18252fec.png"
+  },
+  {
+    "revision": "ff637299194ff52816c61da3aeb3b0df",
+    "url": "/react-portfolio/static/media/Password Generator.ff637299.png"
+  },
+  {
+    "revision": "fe2148ce8a5cc3140c893395feed8ba1",
+    "url": "/react-portfolio/static/media/Profile.fe2148ce.jpg"
+  },
+  {
+    "revision": "20ad750929b529e29d3d6a83ce4268ed",
+    "url": "/react-portfolio/static/media/StreamON.20ad7509.jpg"
+  },
+  {
+    "revision": "4c2efb60108fbc29fc18549bf4643424",
+    "url": "/react-portfolio/static/media/Weather Dashboard.4c2efb60.jpg"
+  },
+  {
+    "revision": "693de46bcb0601be58e76da6a774546c",
+    "url": "/react-portfolio/static/media/Workday Planner.693de46b.png"
+  }
+]);
